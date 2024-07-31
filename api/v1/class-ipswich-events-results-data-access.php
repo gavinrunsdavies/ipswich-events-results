11800fe4d9a50b05e92010515621065324b7987e
Gavin Davies <gavinrunsdavies@gmail.com>
2024-07-31 21:44:51 +0100

    Change API for new DB schema

--- api/v1/class-ipswich-events-results-data-access.php
+++ api/v1/class-ipswich-events-results-data-access.php
@@ -16,186 +16,79 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 namespace IpswichEventResultsAPI\V1;
-	
-require_once plugin_dir_path( __FILE__ ) .'/../config.php';
 
-class Ipswich_Events_Results_Data_Access {		
+require_once plugin_dir_path(__FILE__) . '/../config.php';
+
+class Ipswich_Events_Results_Data_Access
+{
 
 	private $rdb;
 
-	public function __construct() {
-				
-		$this->rdb = new \wpdb(EVENTS_RESULTS_DB_USER, EVENTS_RESULTS_DB_PASSWORD, EVENTS_RESULTS_DB_NAME, DB_HOST);		
+	public function __construct()
+	{
+
+		$this->rdb = new \wpdb(EVENTS_RESULTS_DB_USER, EVENTS_RESULTS_DB_PASSWORD, EVENTS_RESULTS_DB_NAME, DB_HOST);
 		$this->rdb->show_errors();
 	}
-  
-    public function getRaceResults($eventId, $year, $leg) {  	
-      
-      // Ekiden EventId = 4
-      $sql = "
-			SELECT r.position as position, p.name as runnerName, r.result as time, club.name as club, c.code as categoryCode, r.info as info
-			from wp_ije_results r 
-			inner join wp_ije_runners p on r.runner_id = p.id
-			inner join wp_ije_event_course ec on ec.id = r.course_id
-			inner join wp_ije_events e on e.id = ec.event_id
-			inner join wp_ije_clubs club on club.id = r.club_id
-			left outer join wp_ije_sex s on s.id = p.sex_id
-			left outer join wp_ije_category c on r.category_id = c.id
-			left outer join wp_ije_event_division ed on r.event_division_id = ed.id 
-			WHERE e.id = $eventId AND YEAR(r.racedate) = $year AND ed.id = $leg
-			ORDER BY r.position ASC, r.result ASC";
-							
-		$results = $this->rdb->get_results($sql, OBJECT);
 
-		if ($this->rdb->num_rows == 0)
-			return null;
-		
-		if (!$results)	{			
-			return new \WP_Error( 'ipswich_events_results_api_getRaceResults',
-					'Unknown error in reading results from the database', array( 'status' => 500 ) );			
-		}
-
-		return $results;
-	}
-
-  // Gives details of the fields supported for each race, number of results etc
-	public function get_races($event_id) {
-		$sql = "SELECT r.id AS raceId, description, date, course_number as courseNumber, venue, ct.name as courseType,
-    IFNULL(rp.bib_number, 0) as bibNumber,
-    IFNULL(rp.category_position, 0) as categoryPosition,
-    IFNULL(rp.chip_time, 0) as chipTime,
-    IFNULL(rp.gender_position, 0) as genderPosition,
-    IFNULL(rp.gun_time, 0) as gunTime,
-    IFNULL(rp.team_result, 0) as teamResult,
-    IFNULL(rp.single_gender, 0) as singleGenderRace,
-    IFNULL((SELECT 1 FROM `wp_ije_race_prizes` rw WHERE rw.race_id = r.id LIMIT 1), 0) as categoryPrizes,
-    m.id as meetingId,
-    mr.order as meetingRaceOrder,
-    m.name as meetingName, 
-    m.start_date as meetingDate,
-    COUNT(p.id) as numberOfResults
-	  FROM `wp_ije_races` r
-	  INNER JOIN `wp_ije_course_types` ct ON ct.id = r.course_type_id
-    LEFT JOIN `wp_ije_race_properties` rp ON rp.race_id = r.id    
-    LEFT JOIN `wp_ije_meeting_races` mr ON mr.race_id = r.id
-    LEFT JOIN `wp_ije_meetings` m ON m.id = mr.meeting_id
-    LEFT JOIN `wp_ije_results` p ON p.race_id = r.id
-    WHERE r.event_id = $event_id 
-    GROUP BY r.id    
-	  ORDER BY r.date DESC, mr.order ASC";
-
-		return $this->get_results($sql, 'get_races');
-	}
-  
-  	public function get_race($race_id) {
-		$sql = "SELECT r.id AS raceId, description, date, course_number as courseNumber, venue, ct.name as courseType,
-    IFNULL(rp.bib_number, 0) as bibNumber,
-    IFNULL(rp.category_position, 0) as categoryPosition,
-    IFNULL(rp.chip_time, 0) as chipTime,
-    IFNULL(rp.gender_position, 0) as genderPosition,
-    IFNULL(rp.gun_time, 0) as gunTime,
-    IFNULL(rp.team_result, 0) as teamResult,
-    IFNULL(rp.single_gender, 0) as singleGenderRace,
-    COUNT(rw.id) as categoryPrizes,
-    m.id as meetingId,
-    mr.order as meetingRaceOrder,
-    m.name as meetingName, 
-    m.start_date as meetingDate,
-    e.info,
-    e.name
-	  FROM `wp_ije_races` r
-	  INNER JOIN `wp_ije_course_types` ct ON ct.id = r.course_type_id
-    INNER JOIN `wp_ije_events` e ON e.id = r.event_id
-    LEFT JOIN `wp_ije_race_properties` rp ON rp.race_id = r.id
-    LEFT JOIN `wp_ije_race_prizes` rw ON rw.race_id = r.id
-    LEFT JOIN `wp_ije_meeting_races` mr ON mr.race_id = r.id
-    LEFT JOIN `wp_ije_meetings` m ON m.id = mr.meeting_id
-    LEFT JOIN `wp_ije_results` p ON p.race_id = r.id
-    WHERE r.id = $race_id";
-
-		return $this->get_result($sql, 'get_race');
-	}
-
-  // Returns all possible fields
-	public function get_race_results($race_id) {
-		$sql = "SELECT position, name, club, s.sex, s.id as sexId,
-r.bib_number as bibNumber,
-r.category_position as categoryPosition,
-r.chip_time as chipTime,
-r.gender_position as genderPosition,
-r.gun_time as gunTime,
-r.team as team
-		FROM `wp_ije_results` r
-		LEFT JOIN `wp_ije_sex` s ON s.id = r.sex_id        
-		WHERE r.race_id  = $race_id
-    ORDER BY r.position ASC, r.result ASC";
+	public function get_race_results($race_id)
+	{
+		$sql = $this->rdb->prepare('SELECT r.results, m.name, m.date, m.venue FROM `wp_ije_race_results` r INNER JOIN `wp_ije_meetings` m ON m.id = r.meeting_id where r.id=%d', $race_id);
 
 		return $this->get_results($sql, 'get_race_results');
 	}
-  
-    // Returns all possible fields
-	public function get_events() {
-		$sql = "SELECT id, name, info
-		FROM `wp_ije_events` 		
-    ORDER BY name ASC";
+
+	public function get_races($meeting_id)
+	{
+		$sql = $this->rdb->prepare('SELECT r.id, r.name, r.type FROM `wp_ije_race_results` r where r.meeting_id=%d', $meeting_id);
+
+		return $this->get_results($sql, 'get_races');
+	}
+
+	public function get_meetings($event_id)
+	{
+		$sql = $this->rdb->prepare('SELECT id, name, date, venue FROM `wp_ije_meetings` where event_id=%d', $event_id);
+
+		return $this->get_results($sql, 'get_race_results');
+	}
+
+	public function get_events()
+	{
+		$sql = "SELECT id, name, info ROM `wp_ije_events` ORDER BY name ASC";
 
 		return $this->get_results($sql, 'get_events');
 	}
-  
-  	public function get_race_winners($race_id) {
-		$sql = "SELECT r.name, r.club, r.gender_position as genderPosition, IFNULL(r.gun_time, IFNULL(r.chip_time, r.result)) as time, p.sex
-FROM `wp_ije_results` r
-JOIN (
-SELECT s.id as sexId, s.sex, rp.top_finishers as numberOfFinishers
-FROM `wp_ije_race_prizes` rp
-INNER JOIN `wp_ije_sex` s ON rp.sex_id = s.id
-WHERE rp.race_id = $race_id AND rp.category_id IS NULL) p ON. r.race_id = $race_id AND p.sexId = r.sex_id
-where r.gender_position <= p.numberOfFinishers
-ORDER BY sex, genderPosition";
 
-		return $this->get_results($sql, 'get_race_winners');
-	}
-  
-    	public function get_category_winners($race_id) {
-		$sql = "SELECT r.name, r.club, r.category_position as categoryPosition, IFNULL(r.gun_time, IFNULL(r.chip_time, r.result)) as time, p.categoryCode, p.categoryDescription, p.sex
-FROM `wp_ije_results` r
-JOIN (
-SELECT rp.category_id as categoryId, c.code as categoryCode, c.description as categoryDescription, s.id as sexId, s.sex, rp.top_finishers as numberOfFinishers
-FROM `wp_ije_race_prizes` rp
-INNER JOIN `wp_ije_category` c ON rp.category_id = c.id
-INNER JOIN `wp_ije_sex` s ON rp.sex_id = s.id
-WHERE rp.race_id = $race_id) p ON. r.race_id = $race_id AND p.sexId = r.sex_id AND (p.categoryId = r.category_id)
-where r.category_position <= p.numberOfFinishers
-ORDER BY sex, categoryCode, categoryPosition";
-
-		return $this->get_results($sql, 'get_category_winners');
-	}
-
-	private function get_results($sql, $method_name) {
+	private function get_results($sql, $method_name)
+	{
 		$results = $this->rdb->get_results($sql, OBJECT);
 
 		if ($this->rdb->num_rows == 0)
 			return null;
-		
-		if (!$results)	{			
-			return new \WP_Error( 'ipswich_events_results_api_'.$method_name,
-					'Unknown error in reading results from the database', array( 'status' => 500 ) );			
+
+		if (!$results) {
+			return new \WP_Error(
+				'ipswich_events_results_api_' . $method_name,
+				'Unknown error in reading results from the database',
+				array('status' => 500)
+			);
 		}
 
 		return $results;
 	}
 
-	private function get_result($sql, $method_name) {
+	private function get_result($sql, $method_name)
+	{
 		$results = $this->rdb->get_row($sql, OBJECT);
-		
-		if (!$results)	{			
-			return new \WP_Error( 'ipswich_events_results_api_'.$method_name,
-					'Unknown error in reading results from the database', array( 'status' => 500 ) );			
+
+		if (!$results) {
+			return new \WP_Error(
+				'ipswich_events_results_api_' . $method_name,
+				'Unknown error in reading results from the database',
+				array('status' => 500)
+			);
 		}
 
 		return $results;
 	}
-	
-
 }
-?>
--- api/v1/class-ipswich-events-results-wp-rest-api-controller-v1.php
+++ api/v1/class-ipswich-events-results-wp-rest-api-controller-v1.php
@@ -1,164 +1,122 @@
 <?php
-namespace IpswichEventResultsAPI\V1;
-	
-require_once plugin_dir_path( __FILE__ ) .'class-ipswich-events-results-data-access.php';
 
-class Ipswich_Events_Results_WP_REST_API_Controller_V1 {
-	
+namespace IpswichEventResultsAPI\V1;
+
+require_once plugin_dir_path(__FILE__) . 'class-ipswich-events-results-data-access.php';
+
+class Ipswich_Events_Results_WP_REST_API_Controller_V1
+{
+
 	private $data_access;
-	
+
 	private $user;
-	
-	public function __construct() {
+
+	public function __construct()
+	{
 		$this->data_access = new Ipswich_Events_Results_Data_Access();
 	}
-	
-	public function rest_api_init( ) {			
-		
+
+	public function rest_api_init()
+	{
+
 		$namespace = 'ipswich-events-api/v1'; // base endpoint for our custom API
-				
-		$this->register_routes_results($namespace);							
+
+		$this->register_routes_results($namespace);
 	}
-	
-	public function plugins_loaded() {
+
+	public function plugins_loaded()
+	{
 
 		// enqueue WP_API_Settings script
-		add_action( 'wp_print_scripts', function() {
-			wp_enqueue_script( 'wp-api' );
-		} );					
+		add_action('wp_print_scripts', function () {
+			wp_enqueue_script('wp-api');
+		});
 	}
 
-  private function register_routes_results($namespace) {
-	register_rest_route( $namespace, '/events/(?P<eventId>[\d]+)/races/(?P<raceId>[\d]+)/results', array(
-		'methods'             => \WP_REST_Server::READABLE,				
-		'callback'            => array( $this, 'get_race_results' ),
-		'args'                => array(
-			'raceId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
+	private function register_routes_results($namespace)
+	{
+		register_rest_route($namespace, '/events/(?P<eventId>[\d]+)/races/(?P<raceId>[\d]+)/results', array(
+			'methods'             => \WP_REST_Server::READABLE,
+			'callback'            => array($this, 'get_race_results'),
+			'args'                => array(
+				'raceId'           => array(
+					'required'          => true,
+					'validate_callback' => array($this, 'is_valid_id')
 				),
-       'eventId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
+				'eventId'           => array(
+					'required'          => true,
+					'validate_callback' => array($this, 'is_valid_id')
 				)
 			)
-	) );
-  
-  	register_rest_route( $namespace, '/events/(?P<eventId>[\d]+)/races/(?P<raceId>[\d]+)/winners', array(
-		'methods'             => \WP_REST_Server::READABLE,				
-		'callback'            => array( $this, 'get_race_winners' ),
-		'args'                => array(
-			'raceId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
-				),
-      'eventId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
-				)
-			)
-	) );
-  
-    	register_rest_route( $namespace, '/events/(?P<eventId>[\d]+)/races/(?P<raceId>[\d]+)/categorywinners', array(
-		'methods'             => \WP_REST_Server::READABLE,				
-		'callback'            => array( $this, 'get_category_winners' ),
-		'args'                => array(
-			'raceId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
-				),
-      'eventId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
-				)
-			)
-	) );
+		));
 
-		register_rest_route( $namespace, '/events/(?P<eventId>[\d]+)/races', array(
-			'methods'             => \WP_REST_Server::READABLE,				
-			'callback'            => array( $this, 'get_races' ),
-      'args'                => array(
-			'eventId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
+		register_rest_route($namespace, '/events/(?P<eventId>[\d]+)/meetings', array(
+			'methods'             => \WP_REST_Server::READABLE,
+			'callback'            => array($this, 'get_meetings'),
+			'args'                => array(
+				'eventId'           => array(
+					'required'          => true,
+					'validate_callback' => array($this, 'is_valid_id')
 				)
 			)
-		) );		
-    
-    		register_rest_route( $namespace, '/events/(?P<eventId>[\d]+)/races/(?P<raceId>[\d]+)', array(
-			'methods'             => \WP_REST_Server::READABLE,				
-			'callback'            => array( $this, 'get_race' ),
-      'args'                => array(
-			'eventId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
-				),
-      'raceId'           => array(
-				'required'          => true,						
-				'validate_callback' => array( $this, 'is_valid_id' )
+		));
+
+		register_rest_route($namespace, '/events/(?P<eventId>[\d]+)/meetings/(?P<meetingId>[\d]+)/races', array(
+			'methods'             => \WP_REST_Server::READABLE,
+			'callback'            => array($this, 'get_races'),
+			'args'                => array(
+				'eventId'           => array(
+					'required'          => true,
+					'validate_callback' => array($this, 'is_valid_id')
 				)
 			)
-		) );		
+		));
 
-		register_rest_route( $namespace, '/events', array(
-			'methods'             => \WP_REST_Server::READABLE,				
-			'callback'            => array( $this, 'get_events' )
-		) );	    
-	}	
+		register_rest_route($namespace, '/events', array(
+			'methods'             => \WP_REST_Server::READABLE,
+			'callback'            => array($this, 'get_events')
+		));
+	}
 
-	public function get_race_results( \WP_REST_Request $request ) {
+	public function get_race_results(\WP_REST_Request $request)
+	{
 		$response = $this->data_access->get_race_results($request['raceId']);
 
-		return rest_ensure_response( $response );
+		return rest_ensure_response($response);
 	}
-  
-  public function get_race_winners( \WP_REST_Request $request ) {
-		$response = $this->data_access->get_race_winners($request['raceId']);
-    
-    $result = array();
-    $result['male'] = array();
-    $result['female'] = array();
-    foreach ($response as $item) {     
-      if ($item->sex == "Male") {
-        $result['male'][] = $item;
-      } else {
-        $result['female'][] = $item;
-      }      
-    }
-		return rest_ensure_response( $result );
-	}
-  
-    public function get_category_winners( \WP_REST_Request $request ) {
-		$response = $this->data_access->get_category_winners($request['raceId']);
-       
-		return rest_ensure_response( $response );
-	}
-		
-	public function get_races( \WP_REST_Request $request ) {    
-		$response = $this->data_access->get_races($request['eventId']);
 
-		return rest_ensure_response( $response );
-	}		
-  
-  	public function get_race( \WP_REST_Request $request ) {    
-		$response = $this->data_access->get_race($request['raceId']);
+	public function get_meetings(\WP_REST_Request $request)
+	{
+		$response = $this->data_access->get_meetings($request['eventId']);
 
-		return rest_ensure_response( $response );
-	}	
-  
-  	public function get_events( \WP_REST_Request $request ) {    
+		return rest_ensure_response($response);
+	}
+
+	public function get_races(\WP_REST_Request $request)
+	{
+		$response = $this->data_access->get_races($request['meetingId']);
+
+		return rest_ensure_response($response);
+	}
+
+	public function get_events(\WP_REST_Request $request)
+	{
 		$response = $this->data_access->get_events();
 
-		return rest_ensure_response( $response );
-	}	
+		return rest_ensure_response($response);
+	}
 
-	public function is_valid_id( $value, $request, $key ) {
-		if ( $value < 1 ) {
+	public function is_valid_id($value, $request, $key)
+	{
+		if ($value < 1) {
 			// can return false or a custom \WP_Error
-			return new \WP_Error( 'rest_invalid_param',
-				sprintf( '%s %d must be greater than 0', $key, $value ), array( 'status' => 400 ) );
+			return new \WP_Error(
+				'rest_invalid_param',
+				sprintf('%s %d must be greater than 0', $key, $value),
+				array('status' => 400)
+			);
 		} else {
 			return true;
 		}
-	}		
+	}
 }
